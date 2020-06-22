ce4d011e9d8b7b302718ab315f03f6de75c2bc57
“chiru-cd” <cdalmia@cs.iitr.ac.in>
2020-06-22 16:52:00 +0530

    Adding data splitting functionality

--- test_1.py
+++ test_1.py
@@ -1,16 +1,19 @@
 # Currently, the DMLC data parser cannot parse CSV files with headers. Use Pandas to
 # read CSV files with headers.
-
 import pandas as pd
 import numpy as np
 import xgboost as xgb
 import pickle
-import matplotlib.pyplot as plt
+from split import split
 
-# model = xgb.Booster({'nthread': 4})  # init model
-# model.load_model('0003.model')  # load data
-train = pd.read_csv("data/train.csv")
-model = pickle.load(open("test.dat", "rb"))
+print ("Make sure the model & data files are in the working directory")
+model_in = input ("Enter model file (.dat) ")
+data_in = input ("Enter training data file (.csv) ")
+
+split(open('/home/chirag/bounded-gbm/data/' + data_in, 'r'))
+
+data = pd.read_csv('output_1.csv')
+model = pickle.load(open(model_in, "rb"))
 featdict = model.get_fscore()
 for i in featdict:
     print(train[[i]].describe())
