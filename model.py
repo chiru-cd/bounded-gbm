7019c765e9353d3dab0b1e18f361c3f6d5dc68b4
“chiru-cd” <cdalmia@cs.iitr.ac.in>
2020-07-09 12:38:26 +0530

    model

--- model.py
+++ model.py
@@ -9,12 +9,13 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import roc_auc_score
 
-l1 = pd.read_csv("loan_2018Q4.csv")
-l2 = pd.read_csv("loan_2019Q1.csv")
-l3 = pd.read_csv("loan_2019Q2.csv")
-l4 = pd.read_csv("loan_2019Q3.csv")
-# loan = l1.append([l2,l3], ignore_index=True)
-train = l1
+l1 = pd.read_csv("loan_2019Q1.csv")
+l2 = pd.read_csv("loan_2019Q2.csv")
+l3 = pd.read_csv("loan_2019Q3.csv")
+l4 = pd.read_csv("loan_2018Q4.csv")
+loan = l1.append([l2,l3,l4], ignore_index=True)
+loan.to_csv("dev.csv", index=False)
+train = loan
 test = l4
 y_train = train.pop('class')
 X_train = train
@@ -22,8 +23,6 @@ y_test = test.pop('class')
 X_test = test
 
 # X_train, X_test, y_train, y_test = train_test_split( X, y, test_size=0.33, random_state=42, stratify=y)
-
-print(X_train.shape)
 
 X_y_train = xgb.DMatrix(data=X_train, label= y_train)
 X_y_test = xgb.DMatrix(data=X_test)
@@ -42,4 +41,4 @@ model = xgb.train(params=params, dtrain=X_y_train, num_boost_round=500)
 preds =  model.predict(X_y_test)
 
 print (roc_auc_score(y_test, preds))
-pickle.dump(model, open("test.dat", "wb"))
+pickle.dump(model, open("model.dat", "wb"))
